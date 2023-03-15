efd202afd83837d149349c2ca8b9a4b87915c204
Peter Sanford <psanford@sanford.io>
2023-03-15 14:28:24 -0700

    Actually I already had this function defined
    Add an alias for a non-pms prefixed function name.
    
    Default with the current filename. Often I want to just tweak the
    name.

--- dotfiles/emacs/lisp/psanford.el
+++ dotfiles/emacs/lisp/psanford.el
@@ -122,14 +122,17 @@
 
 ;; taken from https://sites.google.com/site/steveyegge2/my-dot-emacs-file
 (defun pms-rename-file-and-buffer (new-name)
- "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
- (let ((name (buffer-name))
-	(filename (buffer-file-name)))
- (if (not filename)
-	(message "Buffer '%s' is not visiting a file!" name)
- (if (get-buffer new-name)
-	 (message "A buffer named '%s' already exists!" new-name)
-	(progn 	 (rename-file name new-name 1) 	 (rename-buffer new-name) 	 (set-visited-file-name new-name) 	 (set-buffer-modified-p nil)))))) ;;
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive (list (read-string "New name: " (file-name-nondirectory (buffer-file-name)))))
+  (let ((name (buffer-name))
+	      (filename (buffer-file-name)))
+    (if (not filename)
+	      (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+	        (message "A buffer named '%s' already exists!" new-name)
+	      (progn 	 (rename-file name new-name 1) 	 (rename-buffer new-name) 	 (set-visited-file-name new-name) 	 (set-buffer-modified-p nil)))))) ;;
+
+(defalias 'rename-file-and-buffer 'pms-rename-file-and-buffer)
 
 ;; taken from https://sites.google.com/site/steveyegge2/my-dot-emacs-file
 (defun pms-move-buffer-file (dir)
