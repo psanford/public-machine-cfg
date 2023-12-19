6eb09ba45ec11617de31438977a5a0c29291906b
Peter Sanford <psanford@sanford.io>
2023-12-19 13:03:59 -0800

    Don't mess with font when not under x11

--- dotfiles/emacs/lisp/jerubbaal.el
+++ dotfiles/emacs/lisp/jerubbaal.el
@@ -1,2 +1,3 @@
-(setq nb-font-height 135)
-(set-face-attribute 'default nil :height nb-font-height)
+(when (eq window-system 'x)
+  (setq nb-font-height 135)
+  (set-face-attribute 'default nil :height nb-font-height))
